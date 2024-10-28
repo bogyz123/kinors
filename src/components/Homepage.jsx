96fc611eb8ecaf98469b552f1715da6d215ba74c
Bogdan <bogdandjakovic123@gmail.com>
2024-10-28 15:14:18 +0100

    new

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import KenoTable from "./KinoTabela"; // Ensure the correct import path for KenoTable
+import KenoTable from "./KinoTabela";
 import KenoAPI from "./KenoAPI";
 
 export default function Homepage() {
@@ -14,7 +14,7 @@ export default function Homepage() {
       API.getTimeForNextGame()
         .then((time) => {
           const inSeconds = Math.floor(time / 1000);
-          setTimeleft(inSeconds + 15); // Check if adding 15 seconds is intended
+          setTimeleft(inSeconds + 15);
         })
         .catch((error) => {
           console.error("Error fetching time for next game:", error);
@@ -24,7 +24,7 @@ export default function Homepage() {
     const getInitialData = () => {
       API.getLastGames(10)
         .then((res) => {
-          setDrawData(res); // Directly set the response instead of spreading it
+          setDrawData(res);
           console.log(res);
         })
         .catch((error) => {
@@ -32,18 +32,17 @@ export default function Homepage() {
         });
     };
 
-    getInitialData(); // Initial fetch for draw data
-    fetchTime(); // Initial fetch for time
+    getInitialData();
+    fetchTime();
 
     interval = setInterval(() => {
       setTimeleft((prevTime) => {
         if (prevTime > 0) {
           return prevTime - 1;
         } else {
-          // When the countdown hits 0, fetch new time and draw data
-          fetchTime(); // Re-fetch time
-          getInitialData(); // Re-fetch last games
-          return 15; // Reset to 15 seconds or any other starting value you want
+          fetchTime();
+          getInitialData();
+          return 15;
         }
       });
     }, 1000);
@@ -56,19 +55,7 @@ export default function Homepage() {
   return (
     <div className="flex flex-col space-y-5 bg-gray-900 text-white h-100 py-5 w-100 border-box font-mont">
       <h1 className="text-center">Poslednja izvlacenja ({timeleft}s)</h1>
-      {drawData &&
-        drawData.map(
-          (draw, index) =>
-            draw.winningNumbers?.list && (
-              <KenoTable
-                last={index === 1} // Check if this is the last element
-                key={draw.drawId}
-                drawId={draw.drawId}
-                drawData={draw.winningNumbers.list || []}
-                snapshot={draw.drawTime}
-              />
-            )
-        )}
+      {drawData && drawData.map((draw, index) => draw.winningNumbers?.list && <KenoTable last={index === 1} key={draw.drawId} drawId={draw.drawId} drawData={draw.winningNumbers.list || []} snapshot={draw.drawTime} />)}
     </div>
   );
 }
